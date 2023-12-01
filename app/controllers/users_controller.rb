dfedda220a04432dceb6cb44982bf23071b0b561
shoutaro <850123.a@gmail.com>
2023-12-01 11:51:04 +0000

    Finish signup-user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params) # user_paramsはparams[:user]の代わりであり、userはuser情報全てを意味する
     if @user.save
-    
+      flash[:success] = '新規作成に成功しました。'
+      redirect_to @user # redirect_to user_url(@user) ←showアクションにリダイレクトしている
     else
       render :new
     end
