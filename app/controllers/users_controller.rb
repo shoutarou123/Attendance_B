d0fc63c928fb356444450dd1029ea6e47c278194
shoutaro <850123.a@gmail.com>
2023-12-01 08:01:12 +0000

    5.5.4終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,19 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params) # user_paramsはparams[:user]の代わりであり、userはuser情報全てを意味する
+    if @user.save
+    
+    else
+      render :new
+    end
+  end
+  
+  private
+  
+    def user_params # 外部のユーザーが知る必要がないため、外部から使用できないようにしている。
+      params.require(:user).permit(:name, :email, :password, :password_confirmation) # requireは必要とする。permitは許可する。
+    end
 end
