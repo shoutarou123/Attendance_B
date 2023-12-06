21ab1caa26cea502742779ac686c9dc31755f7a7
shoutaro <850123.a@gmail.com>
2023-12-06 08:46:23 +0000

    8.2.2終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update] # 共通の@user = User.find(params[:id])をshow,edit,updateアクションで使えるようにしている
+  before_action :logged_in_user, only: [:show, :edit, :update] # ログインユーザーじゃなければ詳細、編集、更新できない
+  before_action :correct_user, only: [:edit, :update] # 現在のユーザーは自分の情報のみ編集可
   
   def show
-    @user = User.find(params[:id])
   end
   
   def new
@@ -20,12 +22,40 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
+  end
+  
+  def update
+    if @user.update(user_params)
+      flash[:success] = 'ユーザー情報を更新しました。'
+      redirect_to @user
+    else
+      render :edit
+    end
   end
   
   private
   
     def user_params # 外部のユーザーが知る必要がないため、外部から使用できないようにしている。
       params.require(:user).permit(:name, :email, :department, :password, :password_confirmation) # requireは必要とする。permitは許可する。
+    end
+    
+    # beforeフィルター
+    
+    # paramsハッシュからユーザーを取得します。
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # ログイン済みのユーザーか確認します。
+    def logged_in_user
+      unless logged_in? # ログインしていなければ下の処理実行
+      flash[:danger] = "ログインしてください。"
+      redirect_to login_url # ログインページへ遷移
+      end
+    end
+    
+    # アクセスしたユーザーが現在ログインしているユーザーか確認します。
+    def correct_user
+      redirect_to(root_url) unless current_user?(@user) # ユーザー情報が現在のユーザーと異なる場合トップページに遷移
     end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,18 +4,48 @@ module SessionsHelper
     session[:user_id] = user.id # userのidレコードをsessionに入れる
   end
   
+  # 永続的セッションを記憶します（Userモデルを参照）
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+  
+  # 永続的セッションを破棄します
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+  
+  # セッションと@current_userを破棄します
   def log_out
+    forget(current_user)
     session.delete(:user_id) # sessionに保持しているuser情報をdeleteする
     @current_user = nil # 現在のユーザー情報が入っている@current_userもnilにする
   end
   
+  # 一時的セッションにいるユーザーを返します。
+  # それ以外の場合はcookiesに対応するユーザーを返します。
   def current_user
-    if session[:user_id] # sessionにuser.idが存在しない場合nilで以下の処理されない。trueで以下が処理される。
+    if (user_id = session[:user_id]) # sessionにuser.idが存在しない場合nilで以下の処理されない。trueで以下が処理される。
       #if @current_user.nil? # 現在のuserがFalse(nil)の時
-      @current_user ||= User.find_by(id: session[:user_id]) # Userモデル内にあるログイン中のuserのuse.idと一致するidの情報を左辺に代入する
+      @current_user ||= User.find_by(id: user_id) # Userモデル内にあるログイン中のuserのuse.idと一致するidの情報を左辺に代入する
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
   
+  # 渡されたユーザーがログイン済みのユーザーであればtrueを返します。
+  def current_user?(user)
+    user == current_user
+  end
+  
+  # 現在ログイン中のユーザーがいればtrue、そうでなければfalseを返します。
   def logged_in?
     !current_user.nil? # 現在ログインしているuserがnilではない？ nilでなかったらture、nilだったらfalse
   end
