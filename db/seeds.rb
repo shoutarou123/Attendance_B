3a56b65ff7d3f78b463879a7bf2241c2673277fd
shoutaro <850123.a@gmail.com>
2023-12-09 08:41:50 +0000

    Finish japanization-and-time-format

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,7 @@
 # 人工ユーザー
 User.create!(name: "Sample User",
              email: "sample@email.com",
+             department: "管理", # 追加
              password: "password",
              password_confirmation: "password",
              admin: true) # 管理権限を持つ
@@ -11,10 +12,11 @@ User.create!(name: "Sample User",
 60.times do |n|
   name  = Faker::Name.name
   email = "sample-#{n+1}@email.com"
+  department = "サンプル" # 追加
   password = "password"
   User.create!(name: name,
                email: email,
-               department: department,
+               department: department, # 追加
                password: password,
                password_confirmation: password)
 end
