35aa05637adcb5c485de74851a3559e82b405a67
shoutaro <850123.a@gmail.com>
2023-12-14 13:16:47 +0000

    10.4終了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,17 +6,19 @@ class ApplicationController < ActionController::Base
 
   # ページ出力前に1ヶ月分のデータの存在を確認・セットします。
   def set_one_month 
-    @first_day = Date.current.beginning_of_month # Date.current当日。beginning_of_month Railsのメソッドで当月の初日。現在の日付が月初を代入
+    @first_day = params[:date].nil?? #nilなら初日をnil以外ならその日を代入
+    Date.current.beginning_of_month : params[:date].to_date# Date.current当日。beginning_of_month Railsのメソッドで当月の初日。現在の日付が月初を代入
     @last_day = @first_day.end_of_month # end_of_month 終日。つまり当月の終日になる。
     one_month = [*@first_day..@last_day] # 対象の月の日数を代入します。showアクションでは使わない為ローカル変数に代入
     # ユーザーに紐付く一ヶ月分のレコードを検索し取得します。
-    @attendances = @user.attendances.where(worked_on: @first_day..@last_day) # showアクションに設定している@user = User.find(params[:id])
-                                                                             # showアクションでも使用することになる為インスタンス変数に代入
+    @attendances = @user.attendances.where(worked_on: @first_day..@last_day).order(:worked_on) # showアクションに設定している@user = User.find(params[:id])
+                                                                             # showアクションでも使用することになる為インスタンス変数に代入。worked_onの値をもとに昇順に並び替えます。
     unless one_month.count == @attendances.count # 対象の月の日数とユーザーの日数が＝ではない場合下の処理実行
       ActiveRecord::Base.transaction do # トランザクションを開始します。
         # 繰り返し処理により、1ヶ月分の勤怠データを生成します。
         one_month.each { |day| @user.attendances.create!(worked_on: day) }
       end
+      @attendances = @user.attendances.where(worked_on: @first_day..@last_day).order(:worked_on) # 2箇所に増やしているのは実際に日付データを繰り返し処理で生成した後にも、正しく@attendances変数に値が代入されるようにするためです。
     end
   
   rescue ActiveRecord::RecordInvalid # トランザクションによるエラーの分岐です。
