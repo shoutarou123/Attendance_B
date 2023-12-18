290387dcf797333bd50261551b5f90ed32d98f45
shoutaro <850123.a@gmail.com>
2023-12-18 12:21:18 +0000

    11.3.1終了

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -8,17 +8,15 @@ class Attendance < ApplicationRecord
   # 出勤時間が存在しない場合、退勤時間は無効
   validate :finished_at_is_invalid_without_a_started_at # オリジナルメソッドのバリデーション
   # 退社時間が出社時間より前の場合、退社時間は無効
-  # validate :date_check # オリジナルメソッドのバリデーション
+  validate :started_at_than_finished_at_fast_if_invalid # オリジナルメソッドのバリデーション
   
   def finished_at_is_invalid_without_a_started_at # オリジナルメソッド
     errors.add(:started_at, "が必要です") if started_at.blank? && finished_at.present? # 出勤時間が空白かつ退勤時間が空白ではないときにメッセージ表示
   end
   
-  # def date_check
-    # if started_at.present?
-      # if finished_at.present? < started_at
-        # errors.add(:finished_at, 'は出社より前の時間を設定することはできません')
-      # end
-    # end
-  # end
+  def started_at_than_finished_at_fast_if_invalid # オリジナルメソッド
+    if started_at.present? && finished_at.present? # 出社、退社どちらも存在すること
+      errors.add(:started_at, "より早い退社は無効です") if started_at > finished_at # 出社の方が大きい(遅い)のは無効
+    end
+  end
 end
